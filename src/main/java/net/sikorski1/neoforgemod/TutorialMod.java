d387b3717b16709a6fb2a6320235902906537577
sikorski1 <kacpersikora.praca@gmail.com>
2025-12-28 23:35:39 +0100

    feat: add bismuth item

--- src/main/java/net/sikorski1/neoforgemod/TutorialMod.java
+++ src/main/java/net/sikorski1/neoforgemod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.sikorski1.neoforgemod;
 
+import net.sikorski1.neoforgemod.items.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -43,7 +44,7 @@ public class TutorialMod {
         modEventBus.addListener(this::commonSetup);
 
         NeoForge.EVENT_BUS.register(this);
-
+        ModItems.register(modEventBus);
         modEventBus.addListener(this::addCreative);
 
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
@@ -55,7 +56,10 @@ public class TutorialMod {
 
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
     }
 
 
---
+++ src/main/java/net/sikorski1/neoforgemod/items/ModItems.java
@@ -0,0 +1,19 @@
+package net.sikorski1.neoforgemod.items;
+
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.sikorski1.neoforgemod.TutorialMod;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TutorialMod.MOD_ID);
+    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
