2fde8e8cb3af2791159617b71ade5b446ffedc7d
sikorski1 <kacpersikora.praca@gmail.com>
2026-01-17 14:05:31 +0100

    feat: create wordgen setup

--- src/main/java/net/sikorski1/neoforgemod/datagen/ModDatapackProvider.java
+++ src/main/java/net/sikorski1/neoforgemod/datagen/ModDatapackProvider.java
@@ -5,10 +5,14 @@ import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.sikorski1.neoforgemod.TutorialMod;
 import net.sikorski1.neoforgemod.enchantment.ModEnchantments;
 import net.sikorski1.neoforgemod.trim.ModTrimMaterials;
 import net.sikorski1.neoforgemod.trim.ModTrimPatterns;
+import net.sikorski1.neoforgemod.worldgen.ModBiomeModifiers;
+import net.sikorski1.neoforgemod.worldgen.ModConfiguredFeatures;
+import net.sikorski1.neoforgemod.worldgen.ModPlacedFeatures;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -17,7 +21,10 @@ public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
             .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap)
-            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap);
+            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap)
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(TutorialMod.MOD_ID));
