0f6a31df87c1da1c180c208dea55432d8cb2a29b
sikorski1 <kacpersikora.praca@gmail.com>
2026-02-15 12:00:25 +0100

    feat: add 3d radiation staff with 2d textures

--- src/main/java/net/sikorski1/neoforgemod/item/ModCreativeModeTabs.java
+++ src/main/java/net/sikorski1/neoforgemod/item/ModCreativeModeTabs.java
@@ -51,6 +51,7 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.GOJI_BERRIES);
                         output.accept(ModItems.GECKO_SPAWN_EGG);
                         output.accept(ModItems.TOMAHAWK);
+                        output.accept(ModItems.RADIATION_STAFF);
                     }).build());
 
     public static final Supplier<CreativeModeTab> BISMUTH_BLOCK_TAB = CREATIVE_MODE_TAB.register("bismuth_blocks_tab",
